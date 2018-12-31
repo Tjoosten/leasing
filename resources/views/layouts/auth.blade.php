cac127951ef25cd8e38fed6136abe4ef991c4d40
Tim Joosten <Tjoosten3@gmail.com>
2018-12-31 03:40:21 +0100

    Center background image

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,50 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- CSRF Token -->
+        <meta charset="UTF-8">
+        <meta name="author" content="{{ config('app.name') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name') }} - @yield('title') </title>
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
-
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        {{-- Styles --}}
+        <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+        <link rel="shortcut icon" type="image/png" href="{{ asset('img/favicon.ico') }}">
     </head>
-    <body>
+    <body class="my-login-page">
         <div id="app">
-            <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-                <a class="navbar-brand mr-auto mr-lg-0" href="#">{{ config('app.name', 'Laravel') }} - Verhuur</a>
-        
-                <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-                    <ul class="navbar-nav mr-auto">
-                        &nbsp;
-                    </ul>
-
-                    <ul class="navbar-nav ml-auto">
-                        
-                    </ul>
-                </div>
-            </nav>
-        
-            
-
-            <main role="main" class="container">
-                <div class="py-4">@yield('content')
-            </main>
+            @yield('content') {{-- Page content --}}
         </div>
+
+        {{-- Javascript --}}
+        <script src="{{ asset('js/auth.js') }}"></script>
     </body>
 </html>
