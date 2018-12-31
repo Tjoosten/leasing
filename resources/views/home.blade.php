5afc78175ebbc072897f81b23afcc7fefe07effd
Tim Joosten <Tjoosten3@gmail.com>
2018-12-31 05:47:40 +0100

    Change the auth layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
    <div class="row">
+        @if (session('status')) {{-- Nodig voor bepaalde handelingen zoals het resetten van een wachtwoord --}}
+            <div class="col-12">
+                <div class="alert alert-success">
+                    <small>{{ session('status') }}</small>
+                </div>
+            </div>
+        @endif
+
         <dashboard-widgets :users="$users"></dashboard-widgets>
     </div>        
 
